6c78c31d11c8b9ffe6e83071c3ec07e9c2f78b5a
Anastasiya(Asya) Pronina <anastasiya.pronina@intel.com>
2023-07-21 13:23:41 +0200

    Updated Open Source version of bitcompactor with latest changes (#2)

--- include/bitCompactor.h
+++ include/bitCompactor.h
@@ -30,124 +30,93 @@
 #include "utils/utils.h"
 #include "utils/logger.h"
 
-/* Define constants for the algorithm here
- *
- */
-#ifndef __BTCMPCTR__H__
-#define __BTCMPCTR__H__
+namespace btc27
+{
+#define BTC27_NUMALGO                 8
+#define BTC27_NUM4KALGO               2
 
-// Block Size
-#define BLKSIZE 64
-#define BIGBLKSIZE 4096
-// Dual Length Mode
-// Indicates if Bit length of the compressed data
-// should be included in the header
-#define DL_INC_BL
-// ALGO
-#define BITC_ALG_NONE 16
-#define LFTSHFTPROC 5
-#define BINEXPPROC 4
-#define BTEXPPROC 6
-#define ADDPROC 3
-#define SIGNSHFTADDPROC 2
-#define NOPROC 0
-#define SIGNSHFTPROC 1
+#define BTC27_MAX_DECOMPRESS_FACTOR   5
+#define BTC27_MAX_COMPRESS_FACTOR     2
 
-#define EOFR 0
-#define CMPRSD 3
-#define UNCMPRSD 2
-#define LASTBLK 1
-// Header bit positions index
-#define BITLN 5
-#define ALGO 2
-
-#define INCR_STATE_UC(state,len)  (*state)++; if(*state == 8) { *state = 0; (*len)++; }
-#define ELEM_SWAP(a,b) { register elem_type t=(a);(a)=(b);(b)=t; }
-
-//-----------------------------------------------------
-//64B block size related constants
-//-----------------------------------------------------
-#define NUMALGO 8
-#define MINPRDCT_IDX 0
-#define MINSPRDCT_IDX 1
-#define MUPRDCT_IDX 2
-#define NOPRDCT_IDX 3
-#define NOSPRDCT_IDX 4
-#define MEDPRDCT_IDX 5
-#define BINCMPCT_IDX 6
-#define BTMAP_IDX 7
-
-#define MAXSYMS 16
-#define NUMSYMSBL 4
-//-----------------------------------------------------
-// 4K Block Size related constants
-//-----------------------------------------------------
-
-#define NUM4KALGO 2
-#define BINCMPCT4K_IDX 0
-#define BTMAP4K_IDX 1
-
-#define MAXSYMS4K 64
-#define NUMSYMSBL4K 6
-
-//-----------------------------------------------------
-#define FILENAME_SIZE 256
-
-
-// Function Declarations
-
-// Alignment
-#define DEFAULT_ALIGN 1
-// 1 --> 32B Alignment
-// 2 --> 64B Alignment
-// 0 --> No  Alignment
-
-#endif
-
-#define BTC_REPORT_INFO(verbosity,lvl,x)  if (lvl <= verbosity) { REPORT_INFO(x); }
-#define BTC_REPORT_ERROR(x) { REPORT_ERROR(x); }
-
-#define BTC_MAX_DECOMPRESS_FACTOR 5
-#define BTC_MAX_COMPRESS_FACTOR 2
+#define BTC27_FILENAME_SIZE           256
 
 class BitCompactor
 {
 public:
 
-    // Arguments typedef.
-    typedef struct btcmpctr_args_s
-    {
-        int cmprs;
-        int decmprs;
-        int blockSize;
-        int superBlockSize;
-        int minFixedBitLn;
-        char inFileName[FILENAME_SIZE];
-        char outFileName[FILENAME_SIZE];
-        int ratio;
-        int verbosity;
-        int mixedBlkSize;
-        int proc_bin_en;   // Enable the Binning preprocessing mode.
-        int proc_btmap_en; // Enable the Bitmap pre-processing mode.
-        int align;    // 0 -> byte align, 1->32B align, 2 -> 64B align.
-        int dual_encode_en; // Enables the dual encoding mode.
-        int bypass_en; // When set to 1, the compressor will treat all blocks as bypass.
-    } btcmpctr_args_t;
-
     // Compress Wrap Arguments
     typedef struct btcmpctr_compress_wrap_args_s
     {
-        int verbosity;
-        int mixedBlkSize;
-        int proc_bin_en;   // Enable the Binning preprocessing mode.
-        int proc_btmap_en; // Enable the Bitmap pre-processing mode.
-        int LblkSize; // Typicall 4096
-        int SblkSize; // Typically 64
-        int align;    // 0 -> byte align, 1->32B align, 2 -> 64B align.
-        int dual_encode_en; // Enables the dual encoding mode.
-        int bypass_en; // When set to 1, the compressor will treat all blocks as bypass.
-        int minFixedBitLn; // Set minimum fixed-length symbol size in bits (0..7, default 3)
+        int verbosity{0};       // Verbosity level
+        int mixedBlkSize{0};    // Enable mixed block size. 0 -> disabled, 1 -> enabled
+        int proc_bin_en{0};     // Enable binning pre-processing mode. 0 -> disabled, 1 -> enabled
+        int proc_btmap_en{0};   // Enable bitmap pre-processing mode. 0 -> disabled, 1 -> enabled
+        int align{1};           // Align mode. 0 -> byte align, 1 -> 32B align, 2 -> 64B align.
+        int dual_encode_en{1};  // Enable dual encoding mode. 0 -> disabled, 1 -> enabled
+        int bypass_en{0};       // When set to 1, the compressor will treat all blocks as bypass.
+        int minFixedBitLn{3};   // Set minimum fixed-length symbol size in bits (0..7, default 3)
     } btcmpctr_compress_wrap_args_t;
+
+    BitCompactor();
+    ~BitCompactor();
+
+    BitCompactor(const BitCompactor &) = delete;
+    BitCompactor& operator= (const BitCompactor &) = delete;
+
+    //      #     ######   ###
+    //     # #    #     #   #
+    //    #   #   #     #   #
+    //   #     #  ######    #
+    //   #######  #         #
+    //   #     #  #         #
+    //   #     #  #        ###
+
+    // BTC decoding/decompression
+    //  return: 1 - decompression success;
+    //          0 - decompression fail;
+    int  DecompressWrap(const unsigned char*        src,            // input compressed buffer data
+                        unsigned int                srcLen,         // input compressed buffer size
+                        unsigned char*              dst,            // output decompressed buffer data
+                        unsigned int&               dstLen,         // input decompressed buffer size bound (Max BTC27_MAX_DECOMPRESS_LEN)
+                                                                    // output decompressed buffer size result
+                        const btcmpctr_compress_wrap_args_t& args   // input decompression configuration args
+                        );
+
+    // BTC encoding/compression
+    //  return: 1 - compression success;
+    //          0 - compression fail;
+    int  CompressWrap(  const unsigned char*        src,            // input decompressed buffer data
+                        unsigned int                srcLen,         // input decompressed buffer size
+                        unsigned char*              dst,            // output compressed buffer data
+                        unsigned int&               dstLen,         // input compressed buffer size bound (see GetCompressedSizeBound)
+                                                                    // output compressed size result
+                        const btcmpctr_compress_wrap_args_t& args   // input compression configuration args
+                        );
+
+    // BTC compressed size bound calculation. Call before compression.
+    //  return: Worst case compressed buffer size for given decompressed buffer size
+    unsigned int GetCompressedSizeBound(unsigned int bufSize        // input decompressed buffer size
+                                        ) const;
+
+    // This is a SWIG/numpy integration friendly interface for the decompression function.
+    //  return: decompressed buffer size result
+    unsigned int DecompressArray(   const unsigned char* const           src,       // input compressed buffer data
+                                    unsigned int                         srcLen,    // input compressed buffer size
+                                    unsigned char*                       dst,       // output decompressed buffer data
+                                    unsigned int                         dstLen,    // input decompressed buffer size bound (Max BTC27_MAX_DECOMPRESS_LEN)
+                                    const btcmpctr_compress_wrap_args_t& args       // input decompression configuration args
+                               );
+
+    // This is a SWIG/numpy integration friendly interface for the compression function.
+    //  return: compressed buffer size result
+    unsigned int CompressArray( const unsigned char* const           src,       // input decompressed buffer data
+                                unsigned int                         srcLen,    // input decompressed buffer size
+                                unsigned char*                       dst,       // output compressed buffer data
+                                unsigned int                         dstLen,    // input compressed buffer size bound (see GetCompressedSizeBound)
+                                const btcmpctr_compress_wrap_args_t& args       // input compression configuration args
+                              );
+
+private:
 
     typedef unsigned char elem_type ;
 
@@ -155,15 +124,15 @@ public:
     //
     typedef struct btcmpctr_algo_args_s
     {
-        unsigned char* inAry;   // Input Buffer to work On.
-        unsigned char* minimum; // Array of bytes to Add or bins
-        unsigned char* bitln;   // Bit Length after the algo is run.
-        unsigned char* residual; // Pre-Processed data to be inserted into the output stream.
-                  int  blkSize;  // Current Block Size to work with.
-                  int* minFixedBitLn;
-                  int* numSyms;  // Number of Symbols binned.
-        unsigned char* bitmap;   // bit map when replacing highest frequency symbol.
-                  int* numBytes; // Number of bytes in residual that is valid, when bitmap compressed.
+        const unsigned char* inAry;   // Input Buffer to work On.
+        unsigned char*       minimum; // Array of bytes to Add or bins
+        unsigned char*       bitln;   // Bit Length after the algo is run.
+        unsigned char*       residual; // Pre-Processed data to be inserted into the output stream.
+                  int        blkSize;  // Current Block Size to work with.
+                  int        minFixedBitLn;
+                  int*       numSyms;  // Number of Symbols binned.
+        unsigned char*       bitmap;   // bit map when replacing highest frequency symbol.
+                  int*       numBytes; // Number of bytes in residual that is valid, when bitmap compressed.
     } btcmpctr_algo_args_t;
 
     // Typedef of the Algo Function pointer.
@@ -183,68 +152,13 @@ public:
     } btcmpctr_algo_choice_t;
 
 
-    BitCompactor();
-    ~BitCompactor();
-
-    BitCompactor(const BitCompactor &) = delete;
-    BitCompactor& operator= (const BitCompactor &) = delete;
-
-    //      #     ######   ###
-    //     # #    #     #   #
-    //    #   #   #     #   #
-    //   #     #  ######    #
-    //   #######  #         #
-    //   #     #  #         #
-    //   #     #  #        ###
-
-    int  DecompressWrap(       unsigned char*  src,
-                               int*   srcLen,
-                               unsigned char*  dst,
-                               int*  dstLen,
-                               btcmpctr_compress_wrap_args_t* args
-                               );
-    int  CompressWrap(         unsigned char* src,
-                               int*           srcLen,
-                               unsigned char* dst,
-                               int*           dstLen,// dstLen holds the size of the output buffer.
-                               btcmpctr_compress_wrap_args_t* args
-                               );
-
-    //This is a SWIG/numpy integration friendly interface for the decompression function.
-    int  DecompressArray(      unsigned char* src,
-                               int   srcLen,
-                               unsigned char* dst,
-                               int   dstLen, // unused
-                               btcmpctr_compress_wrap_args_t* args
-                               );
-    //This is a SWIG/numpy integration friendly interface for the compression function.
-    int  CompressArray(        unsigned char* src,
-                               int   srcLen,
-                               unsigned char* dst,
-                               int   dstLen, // unused
-                               btcmpctr_compress_wrap_args_t* args
-                              );
-
-    void reset();
-
-    unsigned int getDecodeErrorCount ();
-    unsigned int getEncodeErrorCount ();
-
-    btcmpctr_args_t* mBitCompactorConfig;
-
-    int  btcmpctr_cmprs_bound(int bufSize);
-
-    int mVerbosityLevel;
-    std::stringstream mDebugStr;
-private:
-
     // CompressWrap
-    Algo AlgoAry[NUMALGO];
-    Algo AlgoAry4K[NUM4KALGO];
+    Algo AlgoAry[BTC27_NUMALGO];
+    Algo AlgoAry4K[BTC27_NUM4KALGO];
 
     // Declare Array of Algorithms
-    int AlgoAryHeaderOverhead[NUMALGO];
-    int AlgoAryHeaderOverhead4K[NUM4KALGO];
+    int AlgoAryHeaderOverhead[BTC27_NUMALGO];
+    int AlgoAryHeaderOverhead4K[BTC27_NUM4KALGO];
 
     // Returns ceil(log2(i)), i = [0..256]
     const uint8_t mCeilLog2LUT[257] {
@@ -258,9 +172,13 @@ private:
         8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
         8};
 
+    // Debug support
+    int mVerbosityLevel;
+    std::stringstream mDebugStr;
+
     unsigned char btcmpctr_insrt_byte( unsigned char  byte,
                                        unsigned char  bitln,
-                                                int*  outBufLen,
+                                       unsigned int*  outBufLen,
                                        unsigned char* outBuf,
                                        unsigned char  state,
                                        unsigned int*  accum,
@@ -269,7 +187,7 @@ private:
 
     unsigned char btcmpctr_insrt_hdr(int chosenAlgo,
                                      unsigned char  bitln,
-                                              int*  outBufLen,
+                                     unsigned int*  outBufLen,
                                      unsigned char* outBuf,
                                      unsigned char  state,
                                      unsigned int*  accum,
@@ -279,20 +197,20 @@ private:
                                      int            align
                                     );
 
-    void btcmpctr_calc_bitln(unsigned char* residual,
-                             unsigned char* bitln,
-                             int            blkSize,
-                             int*           minFixedBitLn
+    void btcmpctr_calc_bitln(const unsigned char*   residual,
+                             unsigned char*         bitln,
+                             int                    blkSize,
+                             int                    minFixedBitLn
                             );
-    void btcmpctr_calc_dual_bitln( unsigned char* residual,
-                                   unsigned char* bitln,
-                                            int   blkSize,
-                                   unsigned char* bitmap,
-                                            int*  compressedSize
+    void btcmpctr_calc_dual_bitln(const unsigned char* residual,
+                                        unsigned char* bitln,
+                                                 int   blkSize,
+                                        unsigned char* bitmap,
+                                                 int*  compressedSize
                                  );
-    void btcmpctr_tounsigned(signed char* inAry,
-                             unsigned char* residual,
-                             int            blkSize
+    void btcmpctr_tounsigned(const signed char* inAry,
+                             unsigned char*     residual,
+                             int                blkSize
                             );
     void btcmpctr_minprdct(
                             btcmpctr_algo_args_t* algoArg
@@ -322,15 +240,15 @@ private:
                                 btcmpctr_algo_args_t* algoArg
                                );
     void btcmpctr_xtrct_bits(
-                             unsigned char* inBuf,
-                                       int* inBufLen,
+                       const unsigned char* inBuf,
+                             unsigned  int* inBufLen,
                              unsigned char* state,
                              unsigned char* outByte,
                              unsigned char  numBits
                             );
     unsigned char btcmpctr_xtrct_hdr(
-                                     unsigned char* inAry,
-                                               int* inAryPtr,
+                               const unsigned char* inAry,
+                                     unsigned  int* inAryPtr,
                                      unsigned char  state,
                                      unsigned char* cmp,
                                      unsigned char* eofr,
@@ -339,15 +257,15 @@ private:
                                               int*  blkSize,
                                      unsigned char* bytes_to_add, // 1 or 2 bytes
                                      unsigned char* numSyms,
-                                              int*  numBytes,
+                                     unsigned int*  numBytes,
                                      unsigned char* bitmap,
                                               int   mixedBlkSize,
                                               int   dual_encode_en,
                                      unsigned char*  dual_encode
                                     );
     unsigned char btcmpctr_xtrct_bytes_wbitmap(
-                                               unsigned char* inAry,
-                                                         int* inAryPtr,
+                                         const unsigned char* inAry,
+                                               unsigned  int* inAryPtr,
                                                unsigned char  state,
                                                unsigned char  bitln,
                                                unsigned char* outBuf, // Assume OutBuf is BLKSIZE
@@ -355,25 +273,25 @@ private:
                                                unsigned char* bitmap
                                              );
     unsigned char btcmpctr_xtrct_bytes(
-                                        unsigned char* inAry,
-                                                  int* inAryPtr,
+                                  const unsigned char* inAry,
+                                        unsigned  int* inAryPtr,
                                         unsigned char  state,
                                         unsigned char  bitln,
                                         unsigned char* outBuf, // Assume OutBuf is BLKSIZE
                                                   int  blkSize,
                                         unsigned char  mode16
                                       );
-    void btcmpctr_tosigned(unsigned char* inAry,
-                           unsigned char* outBuf
+    void btcmpctr_tosigned(const unsigned char* inAry,
+                                 unsigned char* outBuf
                           );
     void btcmpctr_addByte(
-                          unsigned char* data_to_add,
+                    const unsigned char* data_to_add,
                           unsigned char  mode16,
                           unsigned char* outBuf
                          );
 
 
-    void btcmpctr_initAlgosAry(btcmpctr_compress_wrap_args_t* args);
+    void btcmpctr_initAlgosAry(const btcmpctr_compress_wrap_args_t& args);
 
     unsigned char btcmpctr_getAlgofrmIdx(int idx);
 
@@ -390,4 +308,4 @@ private:
 
     elem_type getMedianNaive(elem_type arr[], unsigned int blkSize);
 };
-
+} // namespace btc27
--- include/utils/logger.h
+++ include/utils/logger.h
@@ -11,34 +11,45 @@
 #include <ostream>
 
 class Logger final {
-	Logger();
-	~Logger();
+    Logger();
+    ~Logger();
 
-	class LoggerImpl;
-	std::unique_ptr<LoggerImpl> mImpl;
+    class LoggerImpl;
+    std::unique_ptr<LoggerImpl> mImpl;
 public:
-	enum class Level : uint8_t {
-		INFO,
-		WARNING,
-		ERROR,
-		FATAL
-	};
+    enum class Level : uint8_t {
+        INFO,
+        WARNING,
+        ERROR,
+        FATAL
+    };
 
-	enum class Verbosity : uint8_t {
-		NONE,
-		LOW,
-		MEDIUM,
-		HIGH
-	};
+    enum class Verbosity : uint8_t {
+        NONE,
+        LOW,
+        MEDIUM,
+        HIGH
+    };
 
-	static Logger& Instance();
+    static Logger& Instance();
 
-	void Info(const char* source, const char* message, Verbosity v = Verbosity::NONE);
-	void Error(const char* source, const char* message);
+    void Info(std::string source, std::string message, Verbosity v = Verbosity::NONE);
+    void Error(std::string source, std::string message);
+
+    std::string ToHexString(const uint16_t value, const uint16_t chars);
+    std::string ToHexString(const uint64_t value);
+    std::string ToDecString(const uint64_t value);
 };
 
-#define REPORT_INFO(x) \
-	do { Logger::Instance().Info("bitCompactor", x, Logger::Verbosity::NONE); } while(false);
+#define REPORT_INFO(source, message, verbosity) \
+    Logger::Instance().Info(source, message, Logger::Verbosity::NONE)
 
-#define REPORT_ERROR(x) \
-	do { Logger::Instance().Error("bitCompactor", x); } while (false);
+#define REPORT_ERROR(source, message) \
+    Logger::Instance().Error(source, message)
+
+#define REPORT_HEX_STR(...) \
+    Logger::Instance().ToHexString(__VA_ARGS__)
+
+#define REPORT_DEC_STR(value) \
+    Logger::Instance().ToDecString(value)
+
