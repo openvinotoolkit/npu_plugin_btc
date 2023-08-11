dad88c0665ea28efaa0c8d1e9047f0646de3ccb2
Anastasiya(Asya) Pronina <anastasiya.pronina@intel.com>
2023-08-11 19:18:37 +0200

    Revert "Updated Open Source version of bitcompactor with latest changes (#2)"
    This reverts commit 6c78c31d11c8b9ffe6e83071c3ec07e9c2f78b5a.

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -6,52 +6,31 @@
 //
 
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <sstream>
 #include "utils/logger.h"
 
 class Logger::LoggerImpl {
 public:
-    LoggerImpl() : mOutput(std::cout) {
-    }
-    std::ostream& mOutput;
+	LoggerImpl() : mOutput(std::cout) {
+	}
+	std::ostream& mOutput;
 };
 
 Logger::Logger():
-    mImpl(new LoggerImpl()) {
+	mImpl(new LoggerImpl()) {
 }
 
 Logger::~Logger() = default;
 
 Logger& Logger::Instance() {
-    static Logger logger;
-    return logger;
+	static Logger logger;
+	return logger;
 }
 
-void Logger::Info(std::string source, std::string message, Verbosity) {
-    mImpl->mOutput << "[REPORT_INFO] <" << source << "> :\t" << message << std::endl;
+void Logger::Info(const char* source, const char* message, Verbosity) {
+	mImpl->mOutput << "[REPORT_INFO] <" << source << "> :\t" << message << std::endl;
 }
 
-void Logger::Error(std::string source, std::string message) {
-    mImpl->mOutput << "[REPORT_ERROR] <" << source << "> :\t" << message << std::endl;
+void Logger::Error(const char* source, const char* message) {
+	mImpl->mOutput << "[REPORT_ERROR] <" << source << "> :\t" << message << std::endl;
 }
 
-std::string Logger::ToDecString(const uint64_t value)
-{
-    return(std::to_string(value));
-}
-
-std::string Logger::ToHexString(const uint64_t value)
-{
-    std::stringstream strStr;
-    strStr << "0x" << std::setfill('0') << std::setw(16) << &std::uppercase << &std::hex << value;
-    return(strStr.str());
-}
-
-std::string Logger::ToHexString(const uint16_t value, const uint16_t chars)
-{
-    std::stringstream strStr;
-    strStr << "0x" << std::setfill('0') << std::setw(chars) << &std::uppercase << &std::hex << value;
-    return(strStr.str());
-}
